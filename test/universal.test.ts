fd9c24247ab4f8d9a9fe16482cec46535d2727f3
Volodymyr Nerovnia <nerv@i.ua>
2024-11-22 00:15:52 +0200

    Updated test for the universal module

--- test/universal.test.ts
+++ test/universal.test.ts
@@ -13,10 +13,10 @@ describe('universal module', () => {
     expect(uniqueArray(arr).length).toBe(4);
   });
 
-  test('array with uniq Date elements length to eual 4', () => {
+  test('array with uniq Date elements length to eual 3', () => {
     const dates = ['11-12-2024','02-10-2007','11-12-2024','08-06-2009','30-06-1994','24-12-2001','25-03-2016','08-06-2009','30-06-1994'];
     const arr = dates.map(date => new Date(date));
-    expect(uniqueArray(arr).length).toBe(6);
+    expect(uniqueArray(arr).length).toBe(3);
   });
   
 
