15559ca2484503427a525cd4a4071626b0ed01a7
Volodymyr Nerovnia <nerv@i.ua>
2024-11-22 00:15:12 +0200

    Updated universal module for the Date type

--- src/universal.ts
+++ src/universal.ts
@@ -1,1 +1,12 @@
-export const uniqueArray = <T>(arr: T[]): T[] => Array.from(new Set<T>(arr));
+export const uniqueArray = <T extends Date | unknown>(arr: T[]): T[] => {
+  if (!Array.isArray(arr) || arr.length === 0) return [];
+  const isDateArray = (array: (Date | unknown)[]): array is Date[] =>
+    array[0] instanceof Date;
+  if (isDateArray(arr)) {
+    const arrMsecFull: number[] = arr.map((date: Date) => date.valueOf());
+    const arrMsec: number[] = arrMsecFull.filter((msec: number) => !(Number.isNaN(msec)));
+    const uniqArr = Array.from(new Set<number>(arrMsec));
+    return uniqArr.map((msec) => new Date(msec)) as T[];
+  }
+  return Array.from(new Set<T>(arr));
+};
