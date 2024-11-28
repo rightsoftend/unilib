3b38874d5ac853cf226286b64cfa166b1aec3e64
Volodymyr Nerovnia <nerv@i.ua>
2024-11-29 00:07:36 +0200

    Added tests for isString function

--- test/strings.test.ts
+++ test/strings.test.ts
@@ -1,10 +1,26 @@
 import {
   generateRandomString,
+  isString,
 } from "../src/strings"
 
 describe('strings module', () => {
   test('random string length to equal 24', () => {
     expect(generateRandomString(24).length).toBe(24);
   });
-
 });
+
+describe('isString - check if the value is a string', () => {
+  test('pass string expect true', () => {
+    expect(isString('lorem')).toBe(true);
+  });
+
+  test('pass number expect false', () => {
+    expect(isString(637)).toBe(false);
+  });
+
+  test('pass object expect false', () => {
+    expect(isString({})).toBe(false);
+  });
+});
+
+
