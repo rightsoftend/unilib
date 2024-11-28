f7bcfd98cc156a6f243dd333e1714bd8d0425c38
Volodymyr Nerovnia <nerv@i.ua>
2024-11-28 23:52:27 +0200

    Created function isString

--- src/strings.ts
+++ src/strings.ts
@@ -1,1 +1,5 @@
-export const generateRandomString = (length: number): string => [...Array(length)].map(() => Math.random().toString(36)[2]).join('')
+export const generateRandomString = (length: number): string => [...Array(length)].map(() => Math.random().toString(36)[2]).join('');
+
+export const isString = (value: unknown): value is string => {
+  return typeof value === 'string';
+};
