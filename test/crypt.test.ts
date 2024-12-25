04abe361f7611624f7c2f1df9fe54f178799bf20
Volodymyr Nerovnia <nerv@i.ua>
2024-12-25 09:45:46 +0200

    Complete tests for crypt module

--- test/crypt.test.ts
+++ test/crypt.test.ts
@@ -1,12 +1,32 @@
 import {
-  hashPassword_sha512,
+  createSaltAndHash,
+  isHashBelongPassword,
 } from "../src/crypt"
 
+const password_1 = 'sdH&g7fgh^Tfgh6-6w';
+const password_2 = 'vc4x,ms&dkj)fhsk';
+const password_3 = '';
+
 describe('crypt module', () => {
+
   test('check if salt and hash are generated', () => {
-    const testHash = hashPassword_sha512('vcx,msdkjfhsk');
+    const testHash = createSaltAndHash(password_1);
     expect(testHash.hash.length).toBe(128);
     expect(testHash.salt.length).toBe(32);
   });
 
+  test('check if generated hash is the same in the both cases for empty password', () => {
+    const testSaltAndHash = createSaltAndHash(password_3);
+    expect(isHashBelongPassword(password_3, testSaltAndHash.salt, testSaltAndHash.hash)).toBe(true);
+  });
+
+  test('check if generated hash is the same in the both cases for not empty password', () => {
+    const testSaltAndHash = createSaltAndHash(password_1);
+    expect(isHashBelongPassword(password_1, testSaltAndHash.salt, testSaltAndHash.hash)).toBe(true);
+  });
+
+  test('check if generated hash is the same in the both cases', () => {
+    const testSaltAndHash = createSaltAndHash(password_1);
+    expect(isHashBelongPassword(password_2, testSaltAndHash.salt, testSaltAndHash.hash)).toBe(false);
+  });
 });
