7f4afadbec9421133e9f3fbaa26f5281ff204e86
Volodymyr Nerovnia <nerv@i.ua>
2024-12-26 07:22:46 +0200

    Added IHashOptions interface to types

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,7 +5,15 @@ interface IHashedPassword {
 
 type THashAlgorithm = 'sha1' | 'sha224' | 'sha256' | 'sha384' | 'sha512' | 'sha3-224' | 'sha3-256' | 'sha3-384' | 'sha3-512' | 'shake128' | 'shake256' | 'rmd160';
 
+interface IHashOptions {
+  algorithm?: THashAlgorithm,
+  iterations?: number, 
+  keyLen?: number, 
+  saltSize?: number,
+}
+
 export {
   IHashedPassword,
   THashAlgorithm,
+  IHashOptions,
 }
