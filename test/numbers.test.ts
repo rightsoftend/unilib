faea14c3234b4359a49234236ca81205a95526ff
Volodymyr Nerovnia <nerv@i.ua>
2024-12-29 20:05:51 +0200

    Made test for function randomNumber

--- test/numbers.test.ts
+++ test/numbers.test.ts
@@ -1,6 +1,20 @@
 import {
   average,
+  randomNumber,
 } from "../src/numbers"
+
+const arrRanges = [
+  {min: -5, max: 2}, 
+  {min: 12, max:12},
+  {min: -200, max:1},
+
+  {min: 2, max: -5}, 
+  {min: 20, max:2},
+  {min: 200, max:-100},
+];
+
+const maxRandomNumberIterations = 2000;
+
 
 describe('numbers module', () => {
   test('calculation average to equal 4', () => {
@@ -8,4 +22,19 @@ describe('numbers module', () => {
     expect(average(...arr)).toBe(4);
   });
 
+  test('get random numbers', () => {
+    for (let i = 0; i < maxRandomNumberIterations; i++) {
+      for (const num of arrRanges) {
+        let min = num.min; let max = num.max;
+        if ( min > max ) [min, max] = [max, min];
+        const rand = randomNumber(min, max);
+        expect(rand >= min && rand <= max).toBe(true);
+      }
+    }
+  });
+
 });
+
+
+
+
