b6b2cb34251b6c3c83391e7823d977b9b28ea67e
Volodymyr Nerovnia <nerv@i.ua>
2024-12-29 20:10:00 +0200

    Created function randomNumber in number module

--- src/numbers.ts
+++ src/numbers.ts
@@ -1,1 +1,11 @@
-export const average = (...args: number[]): number => args.reduce((a, b) => a + b, 0) / args.length;
+const average = (...args: number[]): number => args.reduce((a, b) => a + b, 0) / args.length;
+
+const randomNumber = (min: number, max:number): number => {
+  if ( min > max ) [min, max] = [max, min];
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+export {
+  average,
+  randomNumber,
+};
