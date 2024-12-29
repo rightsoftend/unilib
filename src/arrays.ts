c83349c85b4ccecba46a3562263c03a16577fb43
Volodymyr Nerovnia <nerv@i.ua>
2024-12-29 21:04:44 +0200

    Refactored function getRandomIndex in arrays module

--- src/arrays.ts
+++ src/arrays.ts
@@ -2,12 +2,14 @@ const isStringArray = (arr: unknown[]): arr is string[] => arr.every(item => typ
 
 const isNumberArray = (arr: unknown[]): arr is number[] => arr.every(item => typeof item === 'number');
 
-const getRandomIndex = <T>(arr: T[]): number => Math.floor(Math.random() * arr.length);
+const getRandomIndex = <T>(arr: T[]): number => {
+  if (!(Array.isArray(arr) && arr.length > 0)) return -1;
+  return Math.floor(Math.random() * arr.length);
+}
 
 const numberToArray = (num: number): number[] => [...`${Math.floor(num)}`].map(n => Number.parseInt(n));
 
 const arrayOfNumbersToNumber = (arr: number[]): number => Number(arr.join(''));
-
 
 export {
   isStringArray,
